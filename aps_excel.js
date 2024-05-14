1c7a2b16ae21e138a2f2e453aba7803968ddca11
sdonthula27 <santosh.dontula@gmail.com>
2024-05-14 14:50:27 +0530

    Update aps_excel.js

--- aps_excel.js
+++ aps_excel.js
@@ -152,5 +152,5 @@
             }
         }
     }
-    customElements.define("com-fd-djaja-sap-sac-excel-aps", ExcelAps);
+    customElements.define("com-santhosh-sap-sac-excel", ExcelAps);
 })();
